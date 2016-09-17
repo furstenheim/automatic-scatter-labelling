c93f0d96ef439a506197144939378c4bbb7a480d
gabriel <furstenheim@gmail.com>
2016-09-17 23:51:25 +0200

    Compute relative position of label

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var svg = d3.select('body').append('svg')
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
 d3.csv('data.csv', function (err, data) {
   if (err) throw  err
   data.forEach(function (d) {
@@ -34,4 +35,33 @@ d3.csv('data.csv', function (err, data) {
     .call(d3.axisBottom(x))
   svg.append('g')
     .call(d3.axisLeft(y))
+
+  /* Let's start the label algorithm
+    Only a finite number of angles, 128 is used, we prepare the unit vectors
+   */
+  var vectors
+  for (let i = 0; i< 128; i++) {
+    vectors.push([Math.cos(i * 2 * Math.PI / 128), Math.sin(i * 2 * Math.PI / 128)])
+  }
 })
+
+// Given a bbox and an angle (of the ray coming from the point, We find the relative coordinates of the box. For example if the angle was P/2 the label would be clipped from the corner
+function getRelativeCornerToBBox (bbox, angle) {
+  var verticalDifference = 0
+  var horizontalDifference = 0
+  // Angles grow counterclockwise because height grows from top to bottom
+  if (angle < Math.PI / 4) {
+    verticalDifference = bbox.height * (angle - Math.PI/4) / (Math.PI / 2)
+  } else if (angle  < 3 * Math.PI / 4) {
+    horizontalDifference = -bbox.width * (angle - Math.PI / 4) / (Math.PI / 2)
+  } else if (angle < 5 * Math.PI / 4) {
+    horizontalDifference = -bbox.width
+    verticalDifference = - bbox.height * (angle - 3 * Math.PI / 4) / (Math.PI / 2)
+  } else if (angle < 7 * Math.PI  / 4 ) {
+    verticalDifference = -bbox.height
+    horizontalDifference = -bbox.width * (angle - 7 * Math.PI / 4) / (Math.PI / 2)
+  } else {
+    verticalDifference = bbox.height (angle - 9*Math.PI / 4) / (Math.PI / 2)
+  }
+  return {bottom: verticalDifference, top: verticalDifference, left: horizontalDifference, right: horizontalDifference}
+}
