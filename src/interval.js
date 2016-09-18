16b5b56ac14fe7dd176001f8cc816aafa207c1da
gabriel <furstenheim@gmail.com>
2016-09-18 14:08:20 +0200

    Interval intersection

--- src/interval.js
+++ src/interval.js
@@ -1,4 +1,19 @@
-module.exports = Interval
-function Interval () {
+module.exports = interval
+function Interval (start, end) {
+  if (start > end) {
+    console.error('Wrong order of interval', start, end)
+    return null
+  }
+  this.start = start
+  this.end = end
+  return this
+}
 
+Interval.prototype.intersect = function (interval) {
+  if (this === null || interval === null) return null
+  return new Interval(Math.max(interval.start, this.start), Math.min(interval.end, this.end))
+}
+
+function interval(start, end) {
+  return new Interval(start, end)
 }
