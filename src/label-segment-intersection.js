3ac8517263ad94179976c6a32e4dd3758c6895b7
gabriel <furstenheim@gmail.com>
2016-09-22 18:52:34 +0200

    Add tests for label segment intersection

---
+++ src/label-segment-intersection.js
@@ -0,0 +1,5 @@
+// Find interval in which an interval and a segment intersect
+module.exports = labelSegmentIntersection
+function labelSegmentIntersection (li, vi, pk, vk) {
+
+}
---
+++ test/label-segment-intersection-test.js
@@ -0,0 +1,55 @@
+var interval = require('./../src/interval')
+var labelSegmentIntersection = require('./../src/label-segment-intersection')
+var assert = require('assert')
+
+describe('Label segment Intersection', function () {
+  var tests = [
+    {
+      description: 'Non intersecting',
+      expected: interval.empty(),
+      li: {width: 2, height: 2},
+      vi: {x: 0, y: 1},
+      pk: {x: 2, y: 2},
+      vk: {x:1, y:1}
+    },
+    {
+      description: 'Label containing the point',
+      expected: interval(1, Number.POSITIVE_INFINITY),
+      li: {width: 2, height: 2},
+      vi: {x: 0, y: 1},
+      pk: {x: 0, y: 2},
+      vk: {x:1, y:1}
+    },
+    {
+      description: 'Segment parallel to vector',
+      expected: interval(1, Number.POSITIVE_INFINITY),
+      li: {width: 2, height: 2},
+      vi: {x: 0, y: 1},
+      pk: {x: 0, y: 2},
+      vk: {x:0, y:1}
+    },
+    {
+      description: 'Diagonal vector',
+      expected: interval(1, Number.POSITIVE_INFINITY),
+      li: {width: 2, height: 2},
+      vi: {x: 1, y: 1},
+      pk: {x: 2, y: 2},
+      vk: {x:1, y:1}
+    },
+    {
+      description: 'Diagonal vector, point is not contained',
+      expected: interval(3, 5),
+      li: {width: 2, height: 2},
+      vi: {x: 1, y: 1},
+      pk: {x: 0, y: 4},
+      vk: {x:4, y:0}
+    }
+  ]
+  tests.forEach(function (test) {
+    it(test.description, function () {
+      var result = labelSegmentIntersection(test.li, test.vi, test.pk, test.vk)
+      assert.deepEqual(result, test.expected)
+    })
+  })
+
+})
