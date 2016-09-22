fbc79c631684730026ab6071126550249373675b
gabriel <furstenheim@gmail.com>
2016-09-22 18:38:39 +0200

    Compute segment to segment intersection

---
+++ src/segment-segment-intersection.js
@@ -0,0 +1,12 @@
+module.exports = segmentSegmentIntersection
+// A point pi moves with vi, a segment is defined with pj, vj, we find the time t at which the point intersects and returns parameters s on the segment
+function segmentSegmentIntersection (pi, vi, pj, vj /*Vector of the segment */) {
+  // (vi -vj)(t, s)^T = (pj - pi)
+  var det = - (vi.x* vj.y - vj.x * vi.y)
+  if (det === 0) { // Parallel lines
+    throw new Error('Parallel lines not allowed') // This must be handled out of the algorithm
+  }
+  t = (-(pj.x - pi.x) * vj.y + (pj.y - pi.y) * vj.x) / det
+  s = (-(pj.x - pi.x) * vi.y + (pj.y - pi.y) * vi.x) / det
+  return {t, s}
+}
---
+++ test/segment-segment-intersection-test.js
@@ -0,0 +1,28 @@
+var assert = require('assert')
+var segmentSegmentIntersection = require('./../src/segment-segment-intersection')
+describe('Intersection of vectors', function () {
+  it('Orthogonal vectors', function () {
+    var p1 = {x: 0, y: 1}
+    var v1 = {x: 0, y: 1}
+    var p2 = {x: 3, y: 0}
+    var v2 = {x: 1, y: 0}
+    var intersection = segmentSegmentIntersection(p1, v1, p2, v2)
+    assert.deepEqual(intersection, {t: -1, s: -3})
+  })
+  it('Orthogonal vectors 2', function () {
+    var p2 = {x: 0, y: 1}
+    var v2 = {x: 0, y: 1}
+    var p1 = {x: 3, y: 0}
+    var v1 = {x: 1, y: 0}
+    var intersection = segmentSegmentIntersection(p1, v1, p2, v2)
+    assert.deepEqual(intersection, {t: -3, s: -1})
+  })
+  it('Fourty five degrees', function () {
+    var p1 = {x: 1, y: 1}
+    var v1 = {x: 1, y: 1}
+    var p2 = {x: 3, y: 0}
+    var v2 = {x: 1, y: 0}
+    var intersection = segmentSegmentIntersection(p1, v1, p2, v2)
+    assert.deepEqual(intersection, {t: -1, s: -3})
+  })
+})
