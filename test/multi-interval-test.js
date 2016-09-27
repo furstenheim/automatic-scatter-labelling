5fe8ba5ba295f104632fe4f30aababf3ea874250
gabriel <furstenheim@gmail.com>
2016-09-27 18:38:30 +0200

    Add not equal tests

--- test/multi-interval-test.js
+++ test/multi-interval-test.js
@@ -12,23 +12,29 @@ describe('Creation of intervals', function () {
     {
       description: 'Two intersecting intervals',
       intervals: [interval(0, 2), interval(1, 3)],
-      expected: multiInterval([interval(0, 3)])
+      expected: multiInterval([interval(0, 3)]),
+      notExpected: multiInterval([interval.empty()])
     },
     {
       description: 'Three intervals in two groups',
       intervals: [interval(0, 3), interval(1, 4), interval(5, 6)],
-      expected: multiInterval([interval(0, 4), interval(5, 6)])
+      expected: multiInterval([interval(0, 4), interval(5, 6)]),
+      notExpected: multiInterval([interval(0,6)])
     },
     {
       description: 'Reverse order',
       intervals: [interval(2, 6), interval(-5, -7)],
-      expected: multiInterval([interval(-5, -7), interval(2, 6)])
+      expected: multiInterval([interval(-5, -7), interval(2, 6)]),
+      notExpected: multiInterval([interval(-5, 6)])
     }
   ]
   tests.forEach(function (test) {
     it(test.description, function () {
       var actual = multiInterval(test.intervals)
       assert.deepEqual(actual, test.expected)
+      if (test.notExpected) {
+        assert.notDeepEqual(actual, test.notExpected)
+      }
     })
   })
 })
