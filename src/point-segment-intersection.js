9785174b9b424d9044a37d106b7abbce578b8c43
gabriel <furstenheim@gmail.com>
2016-10-07 19:04:44 +0200

    Add point to segment intersection

--- src/point-segment-intersection.js
+++ src/point-segment-intersection.js
@@ -4,5 +4,12 @@ Given a point and a segment we find the intersection of the ray defined by the s
 module.exports = pointSegmentIntersection
 var interval = require('./interval')
 function pointSegmentIntersection (pi, pk, vk) {
-
+  var det = (pi.x - pk.x) * vk.y - (pi.y - pk.y) * vk.x
+  // point is not contained in the line
+  if (det !== 0) return interval.empty()
+  var intersectionPoint
+  if (vk.y !== 0) intersectionPoint = (pi.y - pk.y) / vk.y
+  else if (vk.x !== 0) intersectionPoint = (pi.x - pk.x) / vk.x
+  if (intersectionPoint < 0) return interval.empty()
+  return  interval(intersectionPoint, Number.POSITIVE_INFINITY)
 }
