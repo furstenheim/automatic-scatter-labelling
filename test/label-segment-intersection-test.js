2db4cc45ba630095b1eb4c3107ad27a3991737b5
gabriel <furstenheim@gmail.com>
2016-10-14 19:49:17 +0200

    Test segment label intersection when point is not the origin

--- test/label-segment-intersection-test.js
+++ test/label-segment-intersection-test.js
@@ -3,54 +3,109 @@ var labelSegmentIntersection = require('./../src/label-segment-intersection')
 var assert = require('assert')
 // TODO test rectangle
 describe('Label segment Intersection', function () {
-  var tests = [
-    {
-      description: 'Non intersecting',
-      expected: interval.empty(),
-      li: {width: 2, height: 2},
-      vi: {x: 0, y: 1},
-      pk: {x: 2, y: 2},
-      vk: {x:1, y:1}
-    },
-    {
-      description: 'Label containing the point',
-      expected: interval(1,4),
-      li: {width: 2, height: 2},
-      vi: {x: 0, y: 1},
-      pk: {x: 0, y: 2},
-      vk: {x:1, y:1}
-    },
-    {
-      description: 'Segment parallel to vector',
-      expected: interval(1, 4),
-      li: {width: 2, height: 2},
-      vi: {x: 0, y: 1},
-      pk: {x: 0, y: 2},
-      vk: {x:0, y:1}
-    },
-    {
-      description: 'Diagonal vector',
-      expected: interval(1, 4),
-      li: {width: 2, height: 2},
-      vi: {x: 1, y: 1},
-      pk: {x: 2, y: 2},
-      vk: {x:1, y:1}
-    },
-    {
-      description: 'Diagonal vector, point is not contained',
-      expected: interval(3, 5),
-      li: {width: 2, height: 2},
-      vi: {x: 1, y: 1},
-      pk: {x: 0, y: 4},
-      vk: {x:4, y:0}
-    }
-  ]
-  tests.forEach(function (test) {
-    it(test.description, function () {
-      var result = labelSegmentIntersection(test.pk, test.vk, test.li, test.vi, {x: 0, y: 0})
-      assert.deepEqual(result, test.expected)
+  describe('Second point at origin', function () {
+    var tests = [
+      {
+        description: 'Non intersecting',
+        expected: interval.empty(),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Label containing the point',
+        expected: interval(1,4),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Segment parallel to vector',
+        expected: interval(1, 4),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:0, y:1}
+      },
+      {
+        description: 'Diagonal vector',
+        expected: interval(1, 4),
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Diagonal vector, point is not contained',
+        expected: interval(3, 5),
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 0, y: 4},
+        vk: {x:4, y:0}
+      }
+    ]
+    tests.forEach(function (test) {
+      it(test.description, function () {
+        var result = labelSegmentIntersection(test.pk, test.vk, test.li, test.vi, {x: 0, y: 0})
+        assert.deepEqual(result, test.expected)
+      })
     })
   })
-
-  // TODO test with origin of label not 0,0
+  describe('Second point not at origin', function () {
+    var tests = [
+      {
+        description: 'Non intersecting',
+        expected: interval.empty(),
+        pi: {x: 0, y: 2},
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1},
+      },
+      {
+        description: 'Label containing the point',
+        expected: interval(1,4),
+        pi: {x: 1, y: 0},
+        li: {width: 3, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Segment parallel to vector',
+        expected: interval(0, 3),
+        pi: {x: 1, y: 1},
+        li: {width: 3, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:0, y:1}
+      },
+      {
+        description: 'Diagonal vector',
+        expected: interval(0, 1),
+        pi: {x: 2, y: 3},
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Diagonal vector, point is not contained',
+        expected: interval(3, 5),
+        pi: {x: -1, y: 0},
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 0, y: 4},
+        vk: {x:4, y:0}
+      }
+    ]
+    tests.forEach(function (test) {
+      it(test.description, function () {
+        var result = labelSegmentIntersection(test.pk, test.vk, test.li, test.vi, test.pi)
+        assert.deepEqual(result, test.expected)
+      })
+    })
+  })
 })
