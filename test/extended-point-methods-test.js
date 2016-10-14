39f338b43c2e0cd11262be8ba76e36d6a73ff16f
gabriel <furstenheim@gmail.com>
2016-10-14 20:15:34 +0200

    Test update minima

--- test/extended-point-methods-test.js
+++ test/extended-point-methods-test.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const extendedPointMethods = require('./../src/extended-point-methods')
 const multiInterval = require('./../src/multi-interval')
 const interval = require('./../src/interval')
+const _ = require('lodash')
 
 describe('Update available space', function () {
   const tests = [
@@ -54,6 +55,66 @@ describe('Update available space', function () {
     it(test.description, function () {
       extendedPointMethods.updateAvailableSpace(test.point)
       assert.equal(test.expected, test.point.availableMeasure)
+    })
+  })
+})
+
+
+describe('Update minima', function () {
+  const tests = [
+    {
+      description: 'No rays',
+      point: {
+        rays: [
+        ]
+      },
+      expected: Number.POSITIVE_INFINITY
+    },
+    {
+      description: 'Empty multiarrays',
+      point: {
+        rays: [
+          {
+            available: multiInterval([interval.empty()])
+          }
+        ]
+      },
+      expected: Number.POSITIVE_INFINITY
+    },
+    {
+      description: 'One multiarray',
+      point: {
+        rays: [
+          {
+            available: multiInterval([interval(0, 4), interval(5, 6)])
+          }
+        ]
+      },
+      expected: 0
+    },
+    {
+      description: 'Two multiarrays',
+      point: {
+        rays: [
+          {
+            available: multiInterval([interval(2, 4), interval(5, 6)])
+          },
+          {
+            available: multiInterval([interval(3, 4), interval(5, 6)])
+          }
+        ]
+      },
+      expected: 2
+    }
+  ]
+  tests.forEach(function (test) {
+    it(test.description, function () {
+      extendedPointMethods.updateMinima(test.point)
+      // If there are no rays there is no much we can test apart from no breaking
+      const minimumRay = _.minBy(test.point.rays, 'minimum')
+      if (minimumRay) {
+        assert.equal(test.expected, minimumRay.minimum)
+      }
     })
   })
 })
