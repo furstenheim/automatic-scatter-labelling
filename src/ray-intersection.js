656058a96da78b58050a7738b74ca5baeb22d34d
gabriel <furstenheim@gmail.com>
2016-11-17 18:22:14 +0100

    Fix ray intersection when it is not possible to label any points in the first go

--- src/ray-intersection.js
+++ src/ray-intersection.js
@@ -6,23 +6,33 @@ const extendedPointMethods = require('./extended-point-methods')
 // Better to grab the module here and fetch the method in the algorithm, that way we can stub
 const labelRectangleIntersection = require('./label-rectangle-intersection')
 const labelSegmentIntersection = require('./label-segment-intersection')
-
+const clone = require('lodash.clone')
 
 // TODO use sets
 function rayIntersection (pointsToLabel, pointsNotToLabel) {
-  var N = []
-  var P = pointsToLabel
+  const rejectedPoints = []
+  // P in the article
+  var remainingPoints = pointsToLabel
   var P0 = pointsToLabel.concat(pointsNotToLabel)
   const pointsLabeled = [] // Here we differ from the original article, once we find a point in P to label we remove it from P and add it to pointsLabeled, otherwise the algorithm does not finish
-  while (P.length !== 0) {
-    let bestRay = findBestRay.findBestRay(P, pointsNotToLabel)
+  while (remainingPoints.length !== 0) {
+    let bestRay = findBestRay.findBestRay(remainingPoints, pointsNotToLabel)
     let rij = bestRay.rbest
     let pi = bestRay.pbest
+    if (rij === undefined) {
+      // It could only happen that we get rij undefined in the first iteration
+      if (pointsLabeled.length !== 0 || rejectedPoints.length !== 0) {
+        throw new Error('Unexpected behaviour')
+      }
+      return {chosen: [], rejected: clone(pointsToLabel)}
+    }
     let vi = {x: rij.vector.x * rij.available.getMin(), y: rij.vector.y * rij.available.getMin()}
     extendedPointMethods.promoteLabelToRectangle(pi, vi)
-    let index = pointsToLabel.findIndex(el => el === pi)
-    P0 = P0.filter((el, i) => i!== index)
-    P = P.filter((el, i) => i!== index)
+    //let index = pointsToLabel.findIndex(el => el === pi)
+    remainingPoints = remainingPoints.filter(el => el !== pi)
+    P0 = P0.filter(el => el !== pi)
+    //P0 = P0.filter((el, i) => i!== index)
+    //P = P.filter((el, i) => i!== index)
     pointsLabeled.push(pi)
     for (let pk of P0) {
       for (let rkl of pk.rays) {
@@ -33,14 +43,13 @@ function rayIntersection (pointsToLabel, pointsNotToLabel) {
       extendedPointMethods.updateAvailableSpace(pk)
 
       // The original article is not very clear here. It removes the point from P but the iteration was on P0. I suppose that if the integral is 0 and the point is in P then it will be removed in the next iteration of the greedy algorithm
-      if (pk.availableMeasure === 0 && P.findIndex(el => el === pk) !== -1){
-        const index = P.findIndex(el => el === pk)
-        P0 = P0.filter((el, i) => i!== index)
-        P = P.filter((el, i) => i!== index)
-        N.push(pk)
+      if (pk.availableMeasure === 0 && remainingPoints.findIndex(el => el === pk) !== -1){
+        P0 = P0.filter(el => el !== pk)
+        remainingPoints = remainingPoints.filter(el => el !== pk)
+        rejectedPoints.push(pk)
       }
     }
   }
 
-  return {N: N, pointsLabeled: pointsLabeled}
+  return {chosen: pointsLabeled, rejected: rejectedPoints}
 }
--- test/ray-intersection-test.js
+++ test/ray-intersection-test.js
@@ -16,7 +16,7 @@ describe('Ray intersection', function () {
     const pointsToLabel = []
     const pointsNotToLabel = []
     const result = rayIntersection(pointsToLabel, pointsNotToLabel)
-    assert.deepEqual(result, {N: [], pointsLabeled: []})
+    assert.deepEqual(result, {rejected: [], chosen: []})
   })
   it('One point to label', function () {
     const pointsToLabel = [
@@ -53,11 +53,11 @@ describe('Ray intersection', function () {
         }
       }
     ]
-    const result = rayIntersection(pointsToLabel, pointsNotToLabel)
+    const {chosen, rejected} = rayIntersection(pointsToLabel, pointsNotToLabel)
     // There is only one ray to label so it is labeled and measure is updated in the not to label
-    assert.equal(result.N.length, 0, 'All rays where accepted')
-    assert.equal(result.pointsLabeled.length, 1, 'Only point was labeled')
-    assert.equal(result.pointsLabeled[0], pointsToLabel[0], 'The only point was labeled')
+    assert.equal(rejected.length, 0, 'All rays where accepted')
+    assert.equal(chosen.length, 1, 'Only point was labeled')
+    assert.equal(chosen[0], pointsToLabel[0], 'The only point was labeled')
   })
   it('Two points stubbed methods', function () {
     const pointsToLabel = [
@@ -112,9 +112,50 @@ describe('Ray intersection', function () {
     sandbox.stub(findBestRay, 'findBestRay', () => { return {pbest: pointsToLabel[1], rbest: pointsToLabel[1].rays[0]} })
     // Intersection is the whole positive axis, this means that the remaining label cannot be displayed
     sandbox.stub(labelSegmentIntersection, 'labelSegmentIntersection', () => interval(0, Number.POSITIVE_INFINITY))
-    const result = rayIntersection(pointsToLabel, pointsNotToLabel)
-    assert.equal(result.pointsLabeled[0], pointsToLabel[1], 'Best ray was stubbed to return second point')
-    assert.equal(result.N[0], pointsToLabel[0], 'intersection algorithms where stubbed to make intersection impossible')
-    assert.deepEqual(result.N[0].rays[0].available, multiInterval.empty(), 'There was no room left')
+    const {chosen, rejected} = rayIntersection(pointsToLabel, pointsNotToLabel)
+    assert.equal(chosen[0], pointsToLabel[1], 'Best ray was stubbed to return second point')
+    assert.equal(rejected[0], pointsToLabel[0], 'intersection algorithms where stubbed to make intersection impossible')
+    assert.deepEqual(rejected[0].rays[0].available, multiInterval.empty(), 'There was no room left')
+  })
+
+  it('One point to label without free space', function () {
+    const pointsToLabel = [
+      {
+        position: {
+          x: 0,
+          y: 0
+        },
+        rays: [
+          {
+            available: multiInterval.empty(),
+            vector: {x: 1, y: 1}
+          }
+        ],
+        label: {
+          height: 1, width: 2
+        }
+      }
+    ]
+    const pointsNotToLabel = [
+      {
+        position: {
+          x: 0,
+          y: 1
+        },
+        rays: [
+          {
+            available: multiInterval([interval(0, Number.POSITIVE_INFINITY)]),
+            vector: {x: 1, y: 1}
+          }
+        ],
+        label: {
+          height: 1, width: 2
+        }
+      }
+    ]
+    const {chosen, rejected} = rayIntersection(pointsToLabel, pointsNotToLabel)
+    // There is only one ray to label so it is labeled and measure is updated in the not to label
+    assert.equal(rejected.length, 1, 'All rays where accepted')
+    assert.equal(chosen.length, 0, 'Only point was labeled')
   })
 })
