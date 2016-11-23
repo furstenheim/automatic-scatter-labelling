cb3018bd9d3d7b7bd73399e60aa3aaebc3215870
gabriel <furstenheim@gmail.com>
2016-11-23 22:12:15 +0100

    Show labels

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ var svg = d3.select('body').append('svg')
 
 d3.csv('data.csv', function (err, data) {
   if (err) throw  err
-  data.forEach(function (d) {
+  data.forEach(function (d, i) {
     d.obesity_percentage = Number(d.obesity_percentage)
     d.life_expectancy_at_60 = Number(d.life_expectancy_at_60)
+    d.id = i
   })
   const xAxis = svg.append('g')
     .attr('transform', `translate(0, ${height})`)
   const yAxis = svg.append('g')
     .attr('class', 'axis-y')
-  render(data.slice(0, 10), xAxis, yAxis)
+  render(data.slice(0, 15), xAxis, yAxis)
 /*  setTimeout(function () {
     render(data.slice(0, 5), xAxis, yAxis)
   }, 2000)*/
@@ -36,21 +37,42 @@ d3.csv('data.csv', function (err, data) {
 function render (data, xAxis, yAxis) {
   x.domain(d3.extent(data, d => d.obesity_percentage)).nice()
   y.domain(d3.extent(data, d => d.life_expectancy_at_60)).nice()
-  const extendedPoints = data.map((d,i) => {
+
+  const labels = svg.selectAll('text.graph-label')
+    .data(data, function (d, i) {
+      return d.id
+    })
+    .enter()
+    .append('text')
+    .attr('class', 'graph-label')
+    .attr('font-size', '20px')
+    .attr('text-anchor', 'middle')
+    .attr('x', d => x(d.obesity_percentage))
+    .attr('y', d => y(d.life_expectancy_at_60))
+    .attr('opacity', 100)
+    .text(function (d) {
+      return d.country
+    }).each(function (d) { // Get sizes for the algorithm
+      var bbox = this.getBBox()
+      d.label = {
+        height: bbox.height,
+        width: bbox.width
+      }
+    })
+
+  const extendedPoints = data.map((d) => {
    return {
-     id: i,
+     id: d.id,
      position: {
        x: x(d.obesity_percentage),
        y: y(d.life_expectancy_at_60)
      },
-     label: {
-       height: 40,
-       width: 40
-     }
+     label: d.label
    }
   })
+  const idToPoints = _.groupBy(extendedPoints, 'id')
   console.log('start ', extendedPoints)
-  debugger
+  //debugger
   const result = mainAlgorithm(extendedPoints)
   console.log(result)
   const dots = svg.selectAll('.dot')
@@ -68,19 +90,34 @@ function render (data, xAxis, yAxis) {
     .attr('opacity', 0)
     .attr('r', 0)
     .remove()
-    /*.on('end', function (){
-      this.remove()
-    })*/
+
   const lines = svg.selectAll('.segment')
     .data(result)
   lines.enter().append('line')
     .attr('class', 'segment')
     .style('stroke', 'black')
     .style('stroke-with', '2px')
-    .attr('x1', d => extendedPoints[d.id].position.x)
-    .attr('y1', d => extendedPoints[d.id].position.y)
+    .attr('x1', d => idToPoints[d.id][0].position.x)
+    .attr('y1', d => idToPoints[d.id][0].position.y)
     .attr('x2', d => (d.rectangle.left + d.rectangle.right) / 2)
     .attr('y2', d => (d.rectangle.top + d.rectangle.bottom) / 2)
+
+  const label2 = svg.selectAll('text.graph-label')
+    .data(result, function (d, i) {
+        return d.id
+    }) // id corresponds to previous position
+
+  label2.transition()
+    .duration(500)
+    .attr('opacity', 100)
+    .attr('x', function (d) {
+      return (d.rectangle.left + d.rectangle.right) / 2
+    })
+    .attr('y', d => (d.rectangle.top + d.rectangle.bottom) / 2)
+  label2.exit()
+    .each(function (d, i){
+      console.log(d, i)
+    }).remove()
   xAxis.call(d3.axisBottom(x))
   yAxis.call(d3.axisLeft(y))
 
