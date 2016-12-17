678e5d6ca20f92b846d8263507f7131abb808159
gabriel <furstenheim@gmail.com>
2016-12-17 19:22:16 +0100

    Adapt label segment tests

--- test/webgl/label-segment-intersection-fragment-test.js
+++ test/webgl/label-segment-intersection-fragment-test.js
@@ -1,13 +1,13 @@
 const webgl = require('./../../src/webgl/webgl')
 const webglUtils = require('./../../src/webgl/utils')
 const mainIntersectionFragment = require('./../../src/webgl/main-intersection-fragment')
-
+const interval = require('./../../src/interval').interval
 const sinon = require('sinon')
 let sandbox
 beforeEach(() => sandbox = sinon.sandbox.create())
 afterEach(() => sandbox.restore())
 // Same tests as in js
-describe('Label segment Intersection', function () {
+describe.only('Label segment Intersection', function () {
   const numberOfRays = 16
   beforeEach(function () {
     sandbox.stub(mainIntersectionFragment, 'mainIntersectionFragment', function () {
@@ -24,40 +24,127 @@ describe('Label segment Intersection', function () {
     })
   })
   describe('Second point at origin', function () {
-    it('Not intersecting', function () {
-      const extendedPoints = [{position: {x: 0, y: 0}, label: {height: 2, width: 2}}, {
-        position: {x: 5, y: 6},
-        label: {width: 4, height: 4}
-      }]
-      sandbox.stub(webglUtils, 'computeRays', function (radiusData) {
-        radiusData[0] = 0
-        radiusData[1] = 1
+    var tests = [
+      {
+        description: 'Non intersecting',
+        expected: interval.empty(),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Label containing the point',
+        expected: interval(1,4),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Segment parallel to vector',
+        expected: interval(1, 4),
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:0, y:1}
+      },
+      {
+        description: 'Diagonal vector',
+        expected: interval(1, 4),
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Diagonal vector, point is not contained',
+        expected: interval(3, 5),
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 0, y: 4},
+        vk: {x:4, y:0}
+      }
+    ]
+    tests.forEach(function (test) {
+      it(test.description, function () {
+        const extendedPoints = [{position: {x: 0, y: 0}, label: test.li}, {position: {x: 4, y: 45}, label: {width: 4, height: 4}}]
+        sandbox.stub(webglUtils, 'computeRays', function (radiusData) {
+          radiusData[0] = test.vi.x
+          radiusData[1] = test.vi.y
+        })
+        const {intersectionData, labelData, radiusData, computeIntersection} = webgl.setUp(extendedPoints, numberOfRays)
+        // rectangle at pk + vk
+        const pk = test.pk
+        const vk = test.vk
+        computeIntersection(pk.y + vk.y, pk.x + vk.x, pk.y + vk.y, pk.x + vk.x, pk.y + vk.y, pk.x, pk.y)
+        assert.deepEqual(interval(intersectionData[0], intersectionData[1]), test.expected, `intersection ${intersectionData[0]}, ${intersectionData[1]} <-> ${test.expected.start} ${test.expected.end}`)
       })
-      const {intersectionData, labelData, radiusData, computeIntersection} = webgl.setUp(extendedPoints, numberOfRays)
-      // rectangle at 3, 3, 3, 3 so the vector goes from 2, 2, to 3, 3
-      computeIntersection(3, 3, 3, 3, 2, 2)
-      console.log(intersectionData.slice(0, 4))
-      assert.equal(intersectionData[0], -1)
-      assert.equal(intersectionData[1], -1)
     })
-    it('Label containing the point', function () {
-      const extendedPoints = [{position: {x: 0, y: 0}, label: {height: 2, width: 2}}, {
-        position: {x: 5, y: 6},
-        label: {width: 4, height: 4}
-      }]
-      sandbox.stub(webglUtils, 'computeRays', function (radiusData) {
-        radiusData[0] = 0
-        radiusData[1] = 1
+  })
+  describe('Second point not at origin', function () {
+    var tests = [
+      {
+        description: 'Non intersecting',
+        expected: interval.empty(),
+        pi: {x: 0, y: 2},
+        li: {width: 2, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1},
+      },
+      {
+        description: 'Label containing the point',
+        expected: interval(1,4),
+        pi: {x: 1, y: 0},
+        li: {width: 3, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Segment parallel to vector',
+        expected: interval(0, 3),
+        pi: {x: 1, y: 1},
+        li: {width: 3, height: 2},
+        vi: {x: 0, y: 1},
+        pk: {x: 0, y: 2},
+        vk: {x:0, y:1}
+      },
+      {
+        description: 'Diagonal vector',
+        expected: interval(0, 1),
+        pi: {x: 2, y: 3},
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 2, y: 2},
+        vk: {x:1, y:1}
+      },
+      {
+        description: 'Diagonal vector, point is not contained',
+        expected: interval(3, 5),
+        pi: {x: -1, y: 0},
+        li: {width: 2, height: 2},
+        vi: {x: 1, y: 1},
+        pk: {x: 0, y: 4},
+        vk: {x:4, y:0}
+      }
+    ]
+    tests.forEach(function (test) {
+      it(test.description, function () {
+        const extendedPoints = [{position: test.pi, label: test.li}, {position: {x: 4, y: 45}, label: {width: 4, height: 4}}]
+        sandbox.stub(webglUtils, 'computeRays', function (radiusData) {
+          radiusData[0] = test.vi.x
+          radiusData[1] = test.vi.y
+        })
+        const {intersectionData, labelData, radiusData, computeIntersection} = webgl.setUp(extendedPoints, numberOfRays)
+        // rectangle at pk + vk
+        const pk = test.pk
+        const vk = test.vk
+        computeIntersection(pk.y + vk.y, pk.x + vk.x, pk.y + vk.y, pk.x + vk.x, pk.y + vk.y, pk.x, pk.y)
+        assert.deepEqual(interval(intersectionData[0], intersectionData[1]), test.expected, `intersection ${intersectionData[0]}, ${intersectionData[1]} <-> ${test.expected.start} ${test.expected.end}`)
       })
-      const {intersectionData, labelData, radiusData, computeIntersection} = webgl.setUp(extendedPoints, numberOfRays)
-      // rectangle without width or height so the vector goes from 0, 2, to 1, 3
-      computeIntersection(3, 1, 3, 1, 0, 2)
-      console.log(intersectionData.slice(0, 4))
-      assert.equal(intersectionData[0], 1)
-      assert.equal(intersectionData[1], 4)
     })
-
     // TODO rest of the tests
   })
-
 })
